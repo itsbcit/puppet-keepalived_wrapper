ee040c014d77ab925f291295fef52a2d59adc14c
Jesse Weisner <jesse@weisner.ca>
2014-11-11 17:44:47 -0800

    moved from profile

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,8 @@
 # keepalived_wrapper
-class keepalived_wrapper {}
+class keepalived_wrapper {
+  # TODO: implement merge behaviour
+
+  $keepalived_global_defs = hiera_hash('keepalived_wrapper::global_defs', {})
+  $keepalived_vrrp_instances = hiera_hash('keepalived_wrapper::vrrp_instances', {})
+  $keepalived_vrrp_scripts = hiera_hash('keepalived_wrapper::vrrp_scripts', {})
+}
